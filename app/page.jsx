9fcea30456a64b398a39047044ea2ecd379af5f3
Sadanand Jaiswal <sadanandjaiswal92@gmail.com>
2024-07-06 12:18:39 +0530

    testing1

--- app/page.jsx
+++ app/page.jsx
@@ -20,7 +20,7 @@ const Home = () => (
         </p>
         <div className='mt-8'>
           <Link href='/quizzes' className='px-8 py-3 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-md transition duration-300'>  
-              Get Started 4
+              Get Started Checking again 1
           </Link>
         </div>
         {/* <MyGaugeChart correct={6} incorrect={2} unanswered={2}/> */}
